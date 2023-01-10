2cdfaa9b4a2a2367e2ba1a72fc91b9526bbb3d26
Davide Di Girolamo <davide.digi2001@gmail.com>
2023-01-10 23:05:28 +0100

    fine palindroma (senza function)

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,3 +1,20 @@
 /* Palidroma
 - Chiedere all'utente di inserire una parola
 - Creare una funzione per capire se la parola inserita è palindroma */
+
+const answer = prompt('Inserisci una parola qualsiasi e scopri se è palindroma!');
+
+let splitted = answer.split('')
+console.log(splitted);
+
+inverted = splitted.reverse();
+console.log(inverted);
+let reunited = splitted.join('');
+console.log(reunited);
+
+if(answer == reunited){
+    console.log('la parola è palindroma');
+  } else {
+    console.log('la parola non è palindroma');
+  }
+  
