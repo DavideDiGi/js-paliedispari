4a4453f0e2e4374ecd691d7834d9adbe1107a347
Davide Di Girolamo <davide.digi2001@gmail.com>
2023-01-11 09:26:52 +0100

    fine esercizio con funzione

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -2,19 +2,23 @@
 - Chiedere all'utente di inserire una parola
 - Creare una funzione per capire se la parola inserita è palindroma */
 
-const answer = prompt('Inserisci una parola qualsiasi e scopri se è palindroma!');
-
-let splitted = answer.split('')
-console.log(splitted);
-
-inverted = splitted.reverse();
-console.log(inverted);
-let reunited = splitted.join('');
-console.log(reunited);
-
-if(answer == reunited){
-    console.log('la parola è palindroma');
-  } else {
-    console.log('la parola non è palindroma');
-  }
+let answer = prompt('Inserisci una parola qualsiasi e scopri se è palindroma!');
   
+function IsPalindrome() {
+
+  let splitted = answer.split('')
+  console.log(splitted);
+
+  inverted = splitted.reverse();
+  console.log(inverted);
+  let reunited = splitted.join('');
+  console.log(reunited);
+
+  if(answer == reunited){
+      prompt('la parola è palindroma');
+    } else {
+      prompt('la parola non è palindroma');
+    }
+}
+
+const palindrome = IsPalindrome();
